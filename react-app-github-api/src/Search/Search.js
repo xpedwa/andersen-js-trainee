2066858e5797de6d841896d81aee7f04e96c08e3
xpedwa <xpedwa@gmail.com>
2021-02-09 15:05:48 +0300

    try send request to github.com

--- react-app-github-api/src/Search/Search.js
+++ react-app-github-api/src/Search/Search.js
@@ -1,12 +1,26 @@
 // eslint-disable-next-line no-use-before-define
 import React, { useState } from 'react';
+import jsonp from 'jsonp';
 import './Search.css';
 
 function searchByGitHub(req) {
-  alert(req);
+  jsonp(
+    `https://github.com/search?q=${req}`, null, (err, data) => {
+      if (err) {
+        console.error(err.message);
+      } else {
+        console.log(data);
+      }
+    },
+  );
+
   fetch(
     `https://github.com/search?q=${req}`,
-  );
+    {
+      mode: 'no-cors',
+    },
+  )
+    .then((response) => console.log(response));
 }
 
 function SearchPanel() {
