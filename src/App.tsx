1d12c722b5c1725f9f0ba967ae07746aff82d32d
xpedwa <xpedwa@gmail.com>
2021-02-18 12:24:04 +0300

    move Search state to Search Component; add cashedQuery to App;

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { Button } from "./Components/defaultComponents";
 import Search from "./Components/Search";
 import Favorites from "./Components/Favorites";
 import View from "./Components/View";
-import { cardInfo } from "./tsType";
 
 function App(): JSX.Element {
-  const [query, setQuery] = useState<string>("");
-  const [page, setPage] = useState<number>(1);
-  const [results, setResults] = useState<cardInfo[]>([]);
-
-  const newSearch = (event: React.FormEvent<HTMLInputElement>) => {
-    setPage(1);
-    setQuery(event.currentTarget.value);
-  };
-
-  const getMore = () => {
-    setPage(page + 1);
-  };
-
-  useEffect(() => {
-    query &&
-      query.length > 2 &&
-      fetch(
-        `https://api.github.com/search/repositories?q=${query}&per_page=20&page=${page}`
-      )
-        .then((response) => response.json())
-        .then((obj) => obj.items)
-        .then((res: cardInfo[]) => {
-          page > 1
-            ? setResults((prevResults: cardInfo[]) => prevResults.concat(res))
-            : setResults(res);
-        });
-  }, [query, page]);
+  const cashedQuery = useState<string>("");
 
   return (
     <Router>
@@ -46,12 +19,7 @@ function App(): JSX.Element {
 
       <Switch>
         <Route path="/search">
-          <Search
-            newSearch={newSearch}
-            query={query}
-            getMore={getMore}
-            results={results}
-          />
+          <Search cashedQuery={cashedQuery} />
         </Route>
 
         <Route
--- src/Components/Search.tsx
+++ src/Components/Search.tsx
@@ -1,19 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button, HotSearchInput, Panel } from "./defaultComponents";
 import ListOfCards from "./ListOfCards";
 import { cardInfo } from "../tsType";
 
 function Search({
-  results,
-  query,
-  newSearch,
-  getMore,
+  cashedQuery,
 }: {
-  results: cardInfo[];
-  query: string;
-  newSearch: (event: React.FormEvent<HTMLInputElement>) => void;
-  getMore: () => void;
+  cashedQuery: [string, React.Dispatch<React.SetStateAction<string>>];
 }): JSX.Element {
+  const [getCashedQuery, setCashedQuery] = cashedQuery;
+
+  const [query, setQuery] = useState<string>(getCashedQuery);
+  const [page, setPage] = useState<number>(1);
+  const [results, setResults] = useState<cardInfo[]>([]);
+
+  const newSearch = (event: React.FormEvent<HTMLInputElement>) => {
+    setPage(1);
+    setQuery(event.currentTarget.value);
+  };
+
+  const getMore = () => {
+    setPage(page + 1);
+  };
+
+  useEffect(() => {
+    setCashedQuery(query);
+
+    query &&
+      query.length > 2 &&
+      fetch(
+        `https://api.github.com/search/repositories?q=${query}&per_page=20&page=${page}`
+      )
+        .then((response) => response.json())
+        .then((obj) => obj.items)
+        .then((res: cardInfo[]) => {
+          page > 1
+            ? setResults((prevResults: cardInfo[]) => prevResults.concat(res))
+            : setResults(res);
+        });
+  }, [query, page]);
+
   return (
     <div>
       <HotSearchInput
